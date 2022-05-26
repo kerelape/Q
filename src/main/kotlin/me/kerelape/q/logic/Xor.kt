1773f54573e7c7fd37252cd4bcf1701abfc81426
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-05-26 13:09:28 +0500

    Create Xor.kt

---
+++ src/main/kotlin/me/kerelape/q/logic/Xor.kt
@@ -0,0 +1,7 @@
+package me.kerelape.q.logic
+
+import me.kerelape.q.Condition
+
+class Xor(private val a: Condition, private val b: Condition) : Condition {
+    override suspend fun value() = Not { this.a.value() == this.b.value() }.value()
+}
