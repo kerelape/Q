cdfd1e25ded6a15e1aa4e899dde2a4ba44624349
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-05-26 13:16:09 +0500

    Create Xnor.kt

---
+++ src/main/kotlin/me/kerelape/q/logic/Xnor.kt
@@ -0,0 +1,7 @@
+package me.kerelape.q.logic
+
+import me.kerelape.q.Condition
+
+class Xnor(private val a: Condition, private val b: Condition) : Condition {
+    override suspend fun value() = Not(Xor(this.a, this.b)).value()
+}
