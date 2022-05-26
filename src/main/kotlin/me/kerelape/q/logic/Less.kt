53afbfe2f46346c03daa0eca9f7052f019df34af
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-05-26 13:03:53 +0500

    Create Less.kt

---
+++ src/main/kotlin/me/kerelape/q/logic/Less.kt
@@ -0,0 +1,9 @@
+package me.kerelape.q.logic
+
+import me.kerelape.q.Condition
+import me.kerelape.q.numbers.QNumber
+import me.kerelape.q.numbers.asInt
+
+class Less<T : Number>(private val a: QNumber<T>, private val b: QNumber<T>) : Condition {
+    override suspend fun value() = this.a.subtract(this.b).asInt().value() < 0
+}
