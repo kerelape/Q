56d697e29a786983bcbb0941a1c440d2d855a4b9
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-05-26 12:59:51 +0500

    Create Greater.kt

---
+++ src/main/kotlin/me/kerelape/q/logic/Greater.kt
@@ -0,0 +1,9 @@
+package me.kerelape.q.logic
+
+import me.kerelape.q.Condition
+import me.kerelape.q.numbers.QNumber
+import me.kerelape.q.numbers.asInt
+
+class Greater<T : Number>(private val a: QNumber<T>, private val b: QNumber<T>) : Condition {
+    override suspend fun value() = this.a.subtract(this.b).asInt().value() > 0
+}
