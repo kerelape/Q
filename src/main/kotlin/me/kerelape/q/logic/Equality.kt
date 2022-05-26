ea1bd14adac27bede50609b30e9a2de7aaf7ff08
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-05-26 13:01:46 +0500

    Update Equality.kt

--- src/main/kotlin/me/kerelape/q/logic/Equality.kt
+++ src/main/kotlin/me/kerelape/q/logic/Equality.kt
@@ -2,7 +2,8 @@ package me.kerelape.q.logic
 
 import me.kerelape.q.Condition
 import me.kerelape.q.numbers.QNumber
+import me.kerelape.q.numbers.asInt
 
 class Equality<T : Number>(private val a: QNumber<T>, private val b: QNumber<T>) : Condition {
-    override suspend fun value() = this.a.subtract(this.b).value() == 0
+    override suspend fun value() = this.a.subtract(this.b).asInt().value() == 0
 }
