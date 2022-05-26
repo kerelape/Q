c10f5f25ff462122400668f9c8cbfff5164ac6d7
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-05-26 11:49:50 +0500

    Add conversion methods

--- src/main/kotlin/me/kerelape/q/text/Text.kt
+++ src/main/kotlin/me/kerelape/q/text/Text.kt
@@ -1,5 +1,19 @@
 package me.kerelape.q.text
 
 import me.kerelape.q.Q
+import me.kerelape.q.numbers.*
 
 typealias Text = Q<String>
+
+fun Text.toByte() = QByte { this.value().toByte() }
+
+fun Text.toShort() = QShort { this.value().toShort() }
+
+fun Text.toInt() = QInt { this.value().toInt() }
+
+fun Text.toLong() = QLong { this.value().toLong() }
+
+fun Text.toFloat() = QFloat { this.value().toFloat() }
+
+fun Text.toDouble() = QDouble { this.value().toDouble() }
+
