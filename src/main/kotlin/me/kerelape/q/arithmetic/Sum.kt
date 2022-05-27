1d8475157728b2bb67d6b9379a05f6bfd1218148
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-05-27 18:54:14 +0500

    Create Sum.kt

---
+++ src/main/kotlin/me/kerelape/q/arithmetic/Sum.kt
@@ -0,0 +1,9 @@
+package me.kerelape.q.arithmetic
+
+import me.kerelape.q.Q
+import me.kerelape.q.Summable
+
+class Sum<in O, out R>(private val augend: Summable<O, R>, private val addend: O) : Q<R> {
+
+    override suspend fun value() = this.augend.add(this.addend).value()
+}
