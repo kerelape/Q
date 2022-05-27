8cf7074c0d82e95d6826c788777cc1640f6a4342
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-05-27 20:01:14 +0500

    Create Reminder.kt

---
+++ src/main/kotlin/me/kerelape/q/arithmetic/Reminder.kt
@@ -0,0 +1,9 @@
+package me.kerelape.q.arithmetic
+
+import me.kerelape.q.Dividable
+import me.kerelape.q.Q
+
+class Reminder<in O, out R>(private val dividend: Dividable<O, R>, private val divisor: O) : Q<R> {
+
+    override suspend fun value() = this.dividend.modulo(this.divisor).value()
+}
