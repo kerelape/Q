dddad56eae3e1ea2a7217b2b3d797917b5051e59
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-05-27 20:34:28 +0500

    Create Stdin.kt

---
+++ src/main/kotlin/me/kerelape/q/io/Stdin.kt
@@ -0,0 +1,9 @@
+package me.kerelape.q.io
+
+import me.kerelape.q.Q
+import java.io.InputStream
+
+object Stdin : Q<InputStream> {
+    
+    override suspend fun value(): InputStream = System.`in`
+}
