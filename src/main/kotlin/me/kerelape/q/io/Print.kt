d7e1a1b56b4d407995d2d52801bbf16106581504
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-05-27 20:21:08 +0500

    Add secondary constructors

--- src/main/kotlin/me/kerelape/q/io/Print.kt
+++ src/main/kotlin/me/kerelape/q/io/Print.kt
@@ -4,7 +4,8 @@ import me.kerelape.q.Q
 import me.kerelape.q.Task
 import java.io.PrintStream
 
-class Print(private val output: Q<PrintStream> = Stdout, private val message: Q<*>) : Task {
+class Print(private val output: Q<PrintStream>, private val message: Q<*>) : Task {
+    constructor(message: Q<*>) : this(Stdout, message)
 
     override suspend fun value() {
         this.output.value().print(this.message.value())
--- src/main/kotlin/me/kerelape/q/io/PrintLine.kt
+++ src/main/kotlin/me/kerelape/q/io/PrintLine.kt
@@ -5,8 +5,9 @@ import me.kerelape.q.Task
 import java.io.PrintStream
 import me.kerelape.q.Sequence
 
-class PrintLine(private val output: Q<PrintStream> = Stdout, private val message: Q<*>) : Task {
-
+class PrintLine(private val output: Q<PrintStream>, private val message: Q<*>) : Task {
+    constructor(message: Q<*>) : this(Stdout, message)
+    
     override suspend fun value() {
         Sequence(
             Print(this.output, this.message),
