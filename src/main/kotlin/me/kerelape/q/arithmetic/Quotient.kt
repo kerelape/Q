1b27f13d7806ae77fc1f34e331c35a82a51b1832
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-05-27 19:08:05 +0500

    Create Quotient.kt

---
+++ src/main/kotlin/me/kerelape/q/arithmetic/Quotient.kt
@@ -0,0 +1,9 @@
+package me.kerelape.q.arithmetic
+
+import me.kerelape.q.Dividable
+import me.kerelape.q.Q
+
+class Quotient<in O, out R>(private val dividend: Dividable<O, R>, private val divisor: O) : Q<R> {
+
+    override suspend fun value() = this.dividend.divide(this.divisor).value()
+}
