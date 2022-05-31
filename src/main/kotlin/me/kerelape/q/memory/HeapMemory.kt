cf06186fb3a4cb99aec37739172d184cac4bb29d
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-06-01 03:11:46 +0500

    Update HeapMemory.kt

--- src/main/kotlin/me/kerelape/q/memory/HeapMemory.kt
+++ src/main/kotlin/me/kerelape/q/memory/HeapMemory.kt
@@ -3,9 +3,9 @@ package me.kerelape.q.memory
 import me.kerelape.q.Q
 import me.kerelape.q.Task
 
-class HeapMemory(private var value: ByteArray?) : Memory<ByteArray> {
+class HeapMemory(private var value: ByteArray = byteArrayOf()) : Memory<ByteArray> {
 
-    override suspend fun value() = this.value ?: throw IllegalStateException("Heap has no value")
+    override suspend fun value() = this.value
 
     override fun write(data: Q<ByteArray>) = Task {
         this.value = data.value()
