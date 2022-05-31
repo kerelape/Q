12d2b55159cc641b5ec9545f95e519a0a6f156ff
Киряев Кирилл <kiryaev.developer@gmail.com>
2022-06-01 03:06:10 +0500

    Update SummableText.kt

--- src/main/kotlin/me/kerelape/q/text/SummableText.kt
+++ src/main/kotlin/me/kerelape/q/text/SummableText.kt
@@ -2,9 +2,7 @@ package me.kerelape.q.text
 
 import me.kerelape.q.Summable
 
-class SummableText(private val origin: Text) : Text, Summable<Text, Text> {
-
-    override suspend fun value() = this.origin.value()
+class SummableText(private val origin: Text) : Text by origin, Summable<Text, Text> {
 
     override fun add(addend: Text) = Text { this.value() + addend.value() }
 }
